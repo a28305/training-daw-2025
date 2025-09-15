b40147fe433ce3daaf88565145bf7a4134b3608a
DESKTOP-481791G\S2-PC00 <licencias@svalero.com>
2025-09-15 18:59:16 +0200

    Clase 15 de septiembre

---
+++ UD1/01_including_script/main.js
@@ -0,0 +1,7 @@
+
+/*<script type="text/javascript">
+ alert("This alert box was called with the onload event");
+</script>
+
+ <script type="text/javascript" src="path-to-javascript-file.js">
+ </script>*/
--- UD1/03_template_strings/main.js
+++ UD1/03_template_strings/main.js
@@ -1,5 +1,7 @@
 function hello(name, lastname, years) {
-}
+    //return "Hola"+ name +""+ lastname+" tienes  "+years+" años de edad"
+    return `Hola ${name} ${lastname}, tienes ${years} años de edad`//(comilla para la izquierda)
+}  
 
 let result = hello('John', 'Anderton', 35)
 if (result != "Hola John Anderton, tienes 35 años de edad") {
